22984e963523ae09dee7cb862661352d6bd03e53
itggot-lukas-lonnbro <lukas.lonnbro@itggot.se>
2018-01-15 09:47:18 +0100

    added user register layout

--- app.rb
+++ app.rb
@@ -5,4 +5,13 @@ class App < Sinatra::Base
     slim :home
   end
 
+  get '/register' do
+    slim :register
+  end
+
+  post '/register' do
+    puts "we did stuff."
+    redirect '/'
+  end
+
 end
